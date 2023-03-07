a6676d41280211cc8d237846c98cf674ffffd4bf
KedarKK1 <officialkedark1@gmail.com>
2023-03-07 20:28:24 +0530

    :sparkles: prediction api-endpoint working
    prediction api-endpoint working

---
+++ README.md
@@ -0,0 +1,13 @@
+# California House Prediction Model
+Steps - 
+```
+venv\Scripts\Activate.bat
+```
+
+```
+pip install -r requirements.txt
+```
+
+```
+flask run
+```
---
+++ app.py
@@ -0,0 +1,44 @@
+import pickle
+from flask import Flask, request, app, jsonify, url_for, render_template
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+
+regmodel = pickle.load(open('regmodel.pkl', 'rb'))
+scalar = pickle.load(open('scaling.pkl', 'rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    data = request.json['data']
+    print("data ", data)
+    print(np.array(list(data.values())).reshape(1, -1))
+    new_data = scalar.transform(np.array(list(data.values())).reshape(1, -1))
+    output = regmodel.predict(new_data)
+    print("output", output[0])  # since o/p is of 2-d array
+    return jsonify(output[0])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# dummy json to api http://127.0.0.1:5000/predict_api-
+# {
+#     "data": {
+#         "MedInc": 8.3252,
+#         "HouseAge": 52.0,
+#         "AveRooms":	6.238137,
+#         "AveBedrms": 1.081081,
+#         "Population": 496.0,
+#         "AveOccup":	2.547945,
+#         "Latitude":	37.88,
+#         "Longitude": -122.22
+#     }
+# }
+# response - 85.75548629729107
