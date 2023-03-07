1e6f76df7b4417b00e886f45fba26caa982b04a6
KedarKK1 <officialkedark1@gmail.com>
2023-03-07 20:40:58 +0530

    :zap: webApp working, html file tested, working done
     webApp working, html file tested, working done

--- app.py
+++ app.py
@@ -25,6 +25,15 @@ def predict_api():
     return jsonify(output[0])
 
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scalar.transform(np.array(data).reshape(1, -1))
+    print("final_input ", final_input)
+    output = regmodel.predict(final_input)[0]
+    return render_template("home.html", prediction_text="The House Price Prediction is {} ".format(output))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
